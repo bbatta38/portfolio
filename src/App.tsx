6901328be29f87641ff02073511243824756c261
bbatta38 <bbatta38@gmail.com>
2019-10-14 21:00:48 +1300

    add one mark

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 
 const App: React.FC = () => {
-  return <div className="App">Hello, Typescript</div>;
+  return <div className="App">Hello, Typescript!</div>;
 };
 
 export default App;
